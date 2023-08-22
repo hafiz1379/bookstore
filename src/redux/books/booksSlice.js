e718ec733ea0bf1eec62411be58f918d8a421270
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-22 16:37:37 +0430

    Update booksSlice with initial state, filter

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,18 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  books: [],
-};
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Da Vinci Code',
+    author: 'Dan Brown',
+    category: 'Action',
+  },
+  {
+    item_id: 'item2',
+    title: 'Good Omens',
+    author: 'Terry Pratchett',
+    category: 'Comedy',
+  },
+  {
+    item_id: 'item3',
+    title: 'George Orwell',
+    author: 'Animal Farm',
+    category: 'Fiction',
+  },
+];
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState,
+  initialState: { books: initialState },
   reducers: {
     addBook: (state, action) => {
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
+      state.books = state.books.filter((book) => book.item_id !== action.payload);
     },
   },
 });
