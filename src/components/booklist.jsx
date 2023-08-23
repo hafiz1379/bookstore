3b606853caba7cf8cac265fc627b4418d6dbb300
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-23 18:13:36 +0430

    Update BookList component with fetchBooks and useEffect

--- src/components/booklist.jsx
+++ src/components/booklist.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from './books';
-import { removeBook } from '../redux/books/booksSlice';
+import { fetchBooks, removeBook } from '../redux/books/booksSlice';
 
 function BookList() {
   const dispatch = useDispatch();
-  const books = useSelector((state) => state.books.books);
+  const books = useSelector((state) => Object.values(state.books).flat());
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   const handleDelete = (id) => {
     dispatch(removeBook(id));
@@ -14,13 +18,13 @@ function BookList() {
   return (
     <div>
       <h2>Book List</h2>
-      {books.map((book) => (
+      {Array.isArray(books) && books.map((book) => (
         <Book
           key={book.item_id}
           title={book.title}
           author={book.author}
           category={book.category}
-          item_id={book.item_id}
+          id={book.item_id !== undefined ? book.item_id : ''}
           handleDelete={() => handleDelete(book.item_id)}
         />
       ))}
